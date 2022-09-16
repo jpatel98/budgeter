1724dd51e879d2082fab44f1bc72347cc0558ca0
Jigar Patel <jigar@JP-machine.local>
2022-09-16 15:16:22 -0400

    Add expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddExpenseForm from './components/AddExpenseForm';
 import Budget from './components/Budget';
 import ExpenseList from './components/ExpenseList';
 import ExpenseTotal from './components/ExpenseTotal';
@@ -23,6 +24,12 @@ function App() {
 		<div className='row mt-3'>
 			<div className='col-sm'>
 				<ExpenseList />
+			</div>
+		</div>
+		<h3 className='mt-3'>Add Expense</h3>
+		<div className='row mt-3'>
+			<div className='col-sm'>
+				<AddExpenseForm />
 			</div>
 		</div>
     </div>
