a45fa94c666d4a0faf9b799f06b63d9c8ff65b86
Jigar Patel <jigar@JP-machine.local>
2022-09-16 12:53:01 -0400

    Add expense list and expense component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
+import ExpenseList from './components/ExpenseList';
 import ExpenseTotal from './components/ExpenseTotal';
 import Remaining from './components/Remaining';
 
 function App() {
   return (
-    <div className='budget'>
-      <h1 className='mt-3'>My Budget Planner</h1>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<Budget />
-					</div>
-					<div className='col-sm'>
-						<Remaining />
-					</div>
-					<div className='col-sm'>
-						<ExpenseTotal />
-					</div>
-				</div>
+    <div className='budget p-4'>
+    	<h1 className='mt-3'>My Budget Planner</h1>
+		<div className='row mt-3'>
+			<div className='col-sm'>
+				<Budget />
+			</div>
+			<div className='col-sm'>
+				<Remaining />
+			</div>
+			<div className='col-sm'>
+				<ExpenseTotal />
+			</div>
+		</div>
+		<h3 className='mt-3'>Expenses</h3>
+		<div className='row mt-3'>
+			<div className='col-sm'>
+				<ExpenseList />
+			</div>
+		</div>
     </div>
   );
 }
