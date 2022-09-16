c5cb6bb60b28e27253a89dd3fe711e4a611972f0
Jigar Patel <jigar@JP-machine.local>
2022-09-16 12:15:50 -0400

    Add heading and budget number components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 
 function App() {
   return (
-    <></>
+    <div className='budget'>
+      <h1 className='mt-3'>My Budget Planner</h1>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<Budget />
+					</div>
+					<div className='col-sm'>
+						<Remaining />
+					</div>
+					<div className='col-sm'>
+						<ExpenseTotal />
+					</div>
+				</div>
+    </div>
   );
 }
 
